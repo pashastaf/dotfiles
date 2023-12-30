27f2193012bfc138f5333f217d7e5a24a2dd50de
pashastaf <pashastaf1@gmail.com>
2023-12-30 20:51:27 +0300

    Arch-up

--- gtk/gtk.sh
+++ gtk/gtk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 gnome_schema="org.gnome.desktop.interface"
-gsettings set "$gnome_schema" icon-theme "Papirus-Dark"
+gsettings set "$gnome_schema" icon-theme "Papirus"
 gsettings set "$gnome_schema" cursor-theme "capitaine-cursors-light"
 gsettings set "$gnome_schema" font-name "Cantarell 11"
 gsettings set "$gnome_schema" color-scheme "prefer-light"
